e74d64309e51a55d2317aa4891a5d444151a3890
hobnob11 <williamhobson62@aol.com>
2015-06-26 06:43:54 +0100

    wow much change

---
+++ screen2.lua
@@ -0,0 +1,10 @@
+os.loadAPI("disk/API/surface")
+local m = peripheral.wrap("top")
+local sw,sh=m.getSize()
+ 
+local d = peripheral.wrap("right")
+local Receivers = {}
+ 
+function UpdateInfo()
+ 
+end
