7669512ec3bc17bc6c25f7249f58277ac1ac47c3
William Hobson <will@willhobson.co.uk>
2016-06-25 09:16:15 +0100

    box function woo

--- controll.lua
+++ controll.lua
@@ -3,21 +3,30 @@ rednet.open("top")
 
 blastdoor = rednet.lookup("frames","blastdoor")
 
-function box(pos,size,color)
-    for Y=1,size.y do 
-        for X=1,size.x do
-            print(X,Y)
-            m.setCursorPos(pos.x+X-1,pos.y+Y-1)
-            m.setTextColor(color)
-            m.setBackgroundColor(color)
+function box(box)
+    for Y=1,box.size.y do 
+        for X=1,box.size.x do
+            m.setCursorPos(box.pos.x+X-1,box.pos.y+Y-1)
+            m.setTextColor(box.color)
+            m.setBackgroundColor(box.color)
             m.write(" ")
         end
     end
+    if box.text then
+        local txt = (#box.text>box.size.x-5 ? string.sub(box.text,1,box.size.x-5).."..." : box.text)
+        local startx = box.pos.x + math.floor(box.size.x/2 - #txt/2)
+        local starty = box.pos.y + math.ceil(box.size.y/2) - 1
+        m.setCursorPos(startx,starty)
+        m.setTextColor(box.textColor)
+        m.write(txt)
 end
 
+m.clear()
 
-box1 = {}
-box1.pos = {x=5,y=2}
-box1.size = {x=10,y=1}
+box1 = {pos={x=5,y=2},size={x=10,y=1},color=colors.red}
 
-box(box1.pos,box1.size,colors.red)
+box(box1)
+
+box2 = {pos={x=5,y=2},size={x=10,y=1},color=colors.blue,text="heh",textColor=colors.white}
+
+box(box2)
