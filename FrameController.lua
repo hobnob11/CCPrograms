2a588a04b4d9f10e3df2037bd6398ff523747e89
William Hobson <will@willhobson.co.uk>
2016-06-25 07:13:04 +0100

    heh

---
+++ FrameController.lua
@@ -0,0 +1,24 @@
+rednet.open("right")
+rednet.host("frames","blastdoor")
+
+--close door side is back
+--open door is top
+-- length is 13
+
+function move(side,dist)
+    for I = 1,dist do 
+        rs.setOutput(side,true)
+        sleep(0.05)
+        rs.setOutput(side,false)
+        sleep(1)
+    end
+end
+
+while true do
+    sender,msg,dist,protc = rednet.receive("frames")
+    if msg == "open" then
+        move("top",13)
+    elseif msg == "close" then
+        move("back",13)
+    end
+end
