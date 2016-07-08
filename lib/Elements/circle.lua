5a4efae71f456512842522c753e9011ebeb2d4eb
Puff-Machine <fluffle83@puffmachine.bugs3.com>
2016-07-08 17:30:37 +0000

    roundifying circle
    dis gonn be good

--- lib/Elements/circle.lua
+++ lib/Elements/circle.lua
@@ -18,6 +18,23 @@ function Circle:_init(x,y,radius,color,startang,endang)
     return self
 end
 
+function pnrg(heh)
+    return (heh < 0) and -heh or heh
+end
+
+function dst(heh1x,heh1y,heh2x,heh2y)
+    return math.sqrt((pnrg(heh1x-heh2x)^2)+(pnrg(heh1y-heh2y)^2))
+
 function Circle:render(Term)
     --INSERT PAF MAGIC HERE
+    --k
+    --https://www.youtube.com/watch?v=Xx-yryFIB6E
+    for Y=1,self.size.y do 
+        for X=1,self.size.x do
+            if dst(X,Y,self.pos.x,self.pos.y) < self.radius
+            then
+                dot(Term,self.pos.x+X-1,self.pos.y+Y-1,self.color)
+            end
+        end
+    end
 end
